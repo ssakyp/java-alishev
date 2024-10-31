42fc28352d5aa2239a7a53167fec7259ad2d0ca4
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 09:49:54 +0500

    Create Strings.java

---
+++ src/Strings.java
@@ -0,0 +1,16 @@
+// primitive and reference types
+// reference type starts with Upper case
+// string is a class
+// classes are written with Upper case
+
+public class Strings {
+  public static void main(String[] args) {
+    int x = 5;
+    String myString = "Hello"; // myString is referenced to "Hello" which is an object
+    String space = " ";
+    String myName = "Bob";
+    System.out.println(myString + space + myName);
+    System.out.println("Hello" + " " + "John");
+    System.out.println("My number is " + x);
+  }
+}
