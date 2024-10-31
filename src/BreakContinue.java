aa3f4173cf18afae7c6d8f012cb53b7638cba034
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 12:28:59 +0500

    Update BreakContinue.java

--- src/BreakContinue.java
+++ src/BreakContinue.java
@@ -1,12 +1,19 @@
 public class BreakContinue {
   public static void main(String[] args) {
-    int i = 0;
-    while(true){
-      if(i==15){
-        break;
+    // int i = 0;
+    // while(true){
+    //   if(i==15){
+    //     break;
+    //   }
+    //   System.out.println(i);
+    //   i++;
+    // }
+    // System.out.println("We are out of loop.");
+    for(int i = 0; i<= 15; i++){
+      if(i%2==0){
+        continue;
       }
-      System.out.println(i);
-      i++;
+      System.out.println("This is an odd number "+i);
     }
   }
 }
