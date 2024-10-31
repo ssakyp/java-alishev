8937ffa2af7b8cd045f1278e1d616a4c95b3fa59
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 09:22:36 +0500

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,8 @@
 // main function is a starting point in java
 // functions are always inside classes
 // anything in Java is located always inside classes
+// after running .java is transferred into a .class file which is a binary file
+// which can be undestood by machines
 
 public class HelloWorld {
   public static void main(String [] args){
