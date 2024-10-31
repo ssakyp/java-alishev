230c944e06caf1de7a59b7ebd698a0c3c3d3b757
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 09:21:15 +0500

    Create HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+// main function is a starting point in java
+// functions are always inside classes
+// anything in Java is located always inside classes
+
+public class HelloWorld {
+  public static void main(String [] args){
+    System.out.println("Hello World!");
+  }
+}
