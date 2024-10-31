9eaf79f90996b33fc3f0ac37ec65782e10552e6e
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 12:46:45 +0500

    Create Arrays.java

---
+++ src/Arrays.java
@@ -0,0 +1,10 @@
+public class Arrays {
+  public static void main(String[] args) {
+    // char c = 'q';  // primitive data type
+    // String s = "Hello";
+    // String s1 = new String("Hello");
+    int num = 10;  // [10]  primitive data type bool, char, int, short, long
+    int[] numbers = new int[5];     // size of array  //numbers -> [array] refernce data type
+    System.out.println(numbers[0]);
+  }
+}
