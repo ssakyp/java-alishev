b88ccd234f0db5ce898d07f2e70bbf566f2ba647
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 10:14:36 +0500

    Update If.java

--- src/If.java
+++ src/If.java
@@ -1,9 +1,9 @@
 public class If {
   public static void main(String[] args) {
-    int myInt = 15;
+    int myInt = 5;
     if(myInt<10){
       System.out.println("yes, correct.");
-    } else if(myInt>20){
+    } else if(myInt<20){
       System.out.println("no, incorrect.");
     } else{
       System.out.println("none of the options");
