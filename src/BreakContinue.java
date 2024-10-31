2f8f431c31e138e924d3e037040c7c7603c309bc
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 12:24:14 +0500

    Update BreakContinue.java
    break operator

--- src/BreakContinue.java
+++ src/BreakContinue.java
@@ -2,6 +2,9 @@ public class BreakContinue {
   public static void main(String[] args) {
     int i = 0;
     while(true){
+      if(i==15){
+        break;
+      }
       System.out.println(i);
       i++;
     }
