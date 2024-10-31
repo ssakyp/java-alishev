b6043d40ff350ec251e9dcc3134d790552beba4a
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 13:14:48 +0500

    Create Multidimensional_arrays.java

---
+++ src/Multidimensional_arrays.java
@@ -0,0 +1,9 @@
+public class Multidimensional_arrays {
+  public static void main(String[] args) {
+    int[] number = {1,2,3};
+    System.out.println(number[1]);
+
+    int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    System.out.println(matrix[2][0]);
+  }
+}
