17a9331ed831cf697a34304db1cb6ea9ac35c1d6
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 12:08:07 +0500

    Create Input.java

---
+++ src/Input.java
@@ -0,0 +1,16 @@
+// String in package java.lang => all classes java.lang are imported in advance
+// Scanner is in java.util
+// System.in - standard flow input data
+
+import java.util.Scanner;
+
+public class Input {
+  public static void main(String[] args) {
+    String s = "string";
+    String new = new String("default way of object initialization");
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter something");
+    String string = scanner.nextLine();    // reads strings of one line until enter is entered
+    System.out.println("You entered: "+string);
+  }
+}
