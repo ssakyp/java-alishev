0e3acc7adf22e6b7394a10b800f308c0f8613ab2
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 13:21:57 +0500

    Update Multidimensional_arrays.java

--- src/Multidimensional_arrays.java
+++ src/Multidimensional_arrays.java
@@ -3,7 +3,25 @@ public class Multidimensional_arrays {
     int[] number = {1,2,3};
     System.out.println(number[1]);
 
-    int[][] matrix = {{1,2,3},{4,5,6},{7,8,9}};
+    int[][] matrix = {{1,2,3},{4,5},{6,7,8,9,10,11,12}};
     System.out.println(matrix[2][0]);
+    System.out.println(matrix[1][1]);
+
+    System.out.println();
+
+    for(int i = 0; i<matrix.length; i++){
+      for(int j = 0; j<matrix[i].length; j++){
+        System.out.println(matrix[i][j] + " ");
+      }
+      System.out.println();
+    }
+
+    
+    int[] numbers = new int[5];
+    Strings[][] strings = new String[2][3];
+    strings[0][1] = "Hello";
+    System.out.println(strings[0][1]);
+
+  
   }
 }
