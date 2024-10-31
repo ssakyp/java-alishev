2233f5cab0b7c184408fd85bd39f0fc3249d68b3
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 10:12:14 +0500

    Create If.java

---
+++ src/If.java
@@ -0,0 +1,10 @@
+public class If {
+  public static void main(String[] args) {
+    int myInt = 15;
+    if(myInt<10){
+      System.out.println("yes, correct.");
+    } else if(myInt>20){
+      System.out.println("no, incorrect.");
+    }
+  }
+}
