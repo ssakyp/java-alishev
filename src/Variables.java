bbcd3d9c4632e17ac62fddf61147dad54b8b9d5a
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 09:36:48 +0500

    Create Variables.java

---
+++ src/Variables.java
@@ -0,0 +1,23 @@
+// int - 32bit; short - 16bit; long - 64bit
+// byte - 8bits -128 - 127
+
+public class Variables {
+  public static void main(String[] args) {
+    int myInt = 785;
+    //myInt = 785;
+    long myLong = 2564654;
+    short myShort = 2561;
+
+    double myDouble = 235.24;
+    float myFloat = 123.1f;
+
+    char c = 'a';
+    boolean b = true;
+
+    byte myByte = 
+    
+    
+    System.out.println(myInt);
+  }
+  
+}
