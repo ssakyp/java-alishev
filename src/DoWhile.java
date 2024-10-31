172da17f7ed782151a1d3ee3e12ee3692e33f58a
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 12:20:05 +0500

    Update DoWhile.java

--- src/DoWhile.java
+++ src/DoWhile.java
@@ -3,12 +3,18 @@ import java.util.Scanner;
 public class DoWhile {
   public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
-    System.out.println("Enter number 5")
-    int value = scanner.nextInt();
-    while(value!=5){
-      System.out.println("Enter number 5")
+    // System.out.println("Enter number 5")
+    // int value = scanner.nextInt();
+    // while(value!=5){
+    //   System.out.println("Enter number 5")
+    //   value = scanner.nextInt();
+    // }
+  
+    int value;
+    do{
+      System.out.println("Please enter 5");
       value = scanner.nextInt();
-    }
-    System.out.println("You entered 5")
+    } while(value!=5);
+    System.out.println("You entered 5");
   }
 }
