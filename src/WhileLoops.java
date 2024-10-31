54feff16b950ebe3ed988a3a27371b29742a9d5b
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 10:01:10 +0500

    Create WhileLoops.java

---
+++ src/WhileLoops.java
@@ -0,0 +1,11 @@
+public class WhileLoops {
+  public void main(String[] args) {
+    boolean t = 5==5;
+    System.out.println(t);
+    int value = 0;
+    while(value<5){
+      System.out.println("Hello "+value);
+      value = value+1;
+    }
+  }
+}
