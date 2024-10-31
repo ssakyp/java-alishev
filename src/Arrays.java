86f4fc40fe9e2b1936f7c3568d21f26a9ed4592d
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 12:55:11 +0500

    Update Arrays.java

--- src/Arrays.java
+++ src/Arrays.java
@@ -5,6 +5,16 @@ public class Arrays {
     // String s1 = new String("Hello");
     int num = 10;  // [10]  primitive data type bool, char, int, short, long
     int[] numbers = new int[5];     // size of array  //numbers -> [array] refernce data type
-    System.out.println(numbers[0]);
+    for(i = 0; i<numbers.length; i++){
+      numbers[i] = i*10
+    }
+    for(i = 0; i<numbers.length; i++){
+      System.out.println(numbers[i]);
+    }
+    System.out.println();
+    int[] numbers1 = {1, 2, 3};
+    for(i = 0; i<numbers1.length; i++){
+      System.out.println(numbers1[i])
+    }
   }
 }
