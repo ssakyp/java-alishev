4c6b517b971687b9d138f9df3815c79d79931ac5
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 10:08:06 +0500

    Update ForLoops.java

--- src/ForLoops.java
+++ src/ForLoops.java
@@ -1,1 +1,10 @@
-
+//for loop
+// iterator; condition; changes
+// i++ same as i = i+1
+public class ForLoops {
+  public static void main(String[] args) {
+    for(int i = 0;i<=10;i++){
+      System.out.println("Hello"+i);
+    }
+  }
+}
