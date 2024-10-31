bf90592f1e88f5adcc97bad96aedf9a6b51fef95
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-10-31 12:15:39 +0500

    Create DoWhile.java

---
+++ src/DoWhile.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class DoWhile {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Enter number 5")
+    int value = scanner.nextInt();
+    while(value!=5){
+      System.out.println("Enter number 5")
+      value = scanner.nextInt();
+    }
+    System.out.println("You entered 5")
+  }
+}
