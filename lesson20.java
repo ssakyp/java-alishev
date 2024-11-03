c140745079a7592cd49eb34b93c32851509c978d
mac <mac@MBP-Adilet.lan>
2024-11-03 10:12:49 +0500

    static

--- lesson20.java
+++ lesson20.java
@@ -1,9 +1,20 @@
 public class lesson20 {
     public static void main(String[] args) {
         Human human1 = new Human("Bob", 22);
-        // human1.setName("Tom");
-        // human1.setAge(12);
-        human1.getInfo();
+        // // human1.setName("Tom");
+        // // human1.setAge(12);
+        // human1.getInfo();
+
+        // Human.description = "Nice";
+        // human1.getAllFields();
+        // Human.getDescription();
+        // Human.description = "Not nice";
+        Human human2 = new Human("Tom", 24);
+        human1.getNumberOfPeople();
+        
+
+        // System.out.println(Math.pow(2, 4));
+        // System.out.println(Math.PI);
     }
 }
 
@@ -13,25 +24,44 @@ class Human {
     // }
     private String name;
     private int age;
-
+    public static String description;
+    private static int countPeople;
+    public static int x;
     // public Human(String name){
     //     System.out.println("Hello from the second constructor");
     //     this.name = name;
     // }
 
-    public Human(){
-        this.name = "Default name";
-        this.age = 0;
-    }
+    // public Human(){
+    //     this.name = "Default name";
+    //     this.age = 0;
+    // }
 
     public Human(String name, int age) {
         System.out.println("Hello from the third constructor");
         this.name = name;
         this.age = age;
+        countPeople++;
     }
     public void getInfo(){
         System.out.println(name+", "+age);
     }
     public void setName(String name) {this.name = name; }
     public void setAge(int age) {this.age = age;}
+
+    public static void getDescription() {
+        System.out.println(description);
+    }
+
+    public void getAllFields(){
+        System.out.println(name+", "+age+", "+description);
+    }
+
+    public static void printAllFields(){
+        System.out.println(description);
+    }
+
+    public void getNumberOfPeople(){
+        System.out.println(countPeople);
+    }
 }
