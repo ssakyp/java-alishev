25d3778098897db6e681c38bed78ed2ca35e501f
mac <mac@MacBook-Pro-Adilet.local>
2024-11-03 12:51:06 +0500

    updated

--- lesson23.java
+++ lesson23.java
@@ -1,16 +1,23 @@
 public class lesson23 {
     public static void main(String[] args){
-        String s = "Hello";
-        String s2 = s.toUpperCase();
-        System.out.println(s + ", " + s2);
+        // String s = "Hello";
+        // String s2 = s.toUpperCase();
+        // System.out.println(s + ", " + s2);
         // String s1 = "Hello";
         // String s3 = " my";
         // String s4 = " friend";
         // String stringAll = s1+s3+s4;
         // System.out.println(stringAll);
-        StringBuilder sb = new StringBuilder("Hello ");
-        System.out.println(sb.toString());
-        sb.append("my").append(" friend");
-        System.out.println(sb.toString());
+        // StringBuilder sb = new StringBuilder("Hello ");
+        // System.out.println(sb.toString());
+        // sb.append("my").append(" friend");
+        // System.out.println(sb.toString());
+        System.out.printf("%fThis is %s a string, %d",2.2, "NICE",24);
+        System.out.printf("String %10d\n", 534);
+        System.out.printf("String %10d\n", 5);
+        System.out.printf("String %10d\n", 5340);
+        System.out.printf("String %10d\n", 53400);
+        System.out.printf("String %10d\n", 5340000);
+        System.out.printf("The floating point number: %.2f\n", 2.222224);
     }
 }
