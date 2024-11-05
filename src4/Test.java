2d09cd251171c0b2f143e7a506c1c8b0cea14c87
ssakyp <sultansakyp77@gmail.com>
2024-11-05 07:00:21 +0500

    primitive class wrap

---
+++ src4/Test.java
@@ -0,0 +1,10 @@
+package src4;
+
+public class Test {
+    public static void main(String[] args) {
+        int x = 1;
+        Integer y = x;
+        int x2 = y;
+        Integer.parseInt("123");
+    }
+}
