95437740e5b7301e6d31e461013e0e357c4836ee
ssakyp <sultansakyp77@gmail.com>
2024-11-06 08:26:19 +0500

    lesson 46 serialization of array of objects

--- src18/ReadObject.java
+++ src18/ReadObject.java
@@ -4,16 +4,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.ObjectInputStream;
+import java.util.Arrays;
 
 public class ReadObject {
     public static void main(String[] args) {
         try {
             FileInputStream fis = new FileInputStream("people.bin");
             ObjectInputStream ois = new ObjectInputStream(fis);
-            Person person1 = (Person) ois.readObject();
-            Person person2 = (Person) ois.readObject();
-            System.out.println(person1);
-            System.out.println(person2);
+//            int personCount = ois.readInt();
+//            Person[] people = new Person[personCount];
+//            for(int i = 0; i < personCount; i++){
+//                people[i] = (Person) ois.readObject();
+//            }
+            Person[] people = (Person[]) ois.readObject();
+//            Person person1 = (Person) ois.readObject();
+//            Person person2 = (Person) ois.readObject();
+//            System.out.println(person1);
+//            System.out.println(person2);
+            System.out.println(Arrays.toString(people));
             ois.close();
         } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
--- src18/WriteObject.java
+++ src18/WriteObject.java
@@ -6,13 +6,19 @@ public class WriteObject {
     public static void main(String[] args) {
         Person person1 = new Person(1, "Bob");
         Person person2 = new Person(2, "Mike");
+        Person[] people = {new Person(1, "Bob"), new Person(2, "Mike"), new Person(3, "Jack")};
 
         try {
             FileOutputStream fos = new FileOutputStream("people.bin");
             ObjectOutputStream oos = new ObjectOutputStream(fos);
 
-            oos.writeObject(person1);
-            oos.writeObject(person2);
+//            oos.writeInt(people.length);
+//            for(Person person : people){
+//                oos.writeObject(person);
+//            }
+            oos.writeObject(people);
+//            oos.writeObject(person1);
+//            oos.writeObject(person2);
             oos.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
